a0884f7519b2cb0fa04fc11080ffb75ccc899309
boyhp0079 <boyhp0077@gmail.com>
2020-11-16 11:14:55 +0700

    dddd

--- client/src/Component/FooterPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../assets/footer.scss'
-export default function FooterPage() {
-    return (
-        <div className="footer">
-            <h3>Footer</h3>
-        </div>
-    )
-}
--- client/src/Component/index.js
+++
@@ -1,6 +0,0 @@
-import Footer from './FooterPage'
-import Header from './Header'
-import ChatBox from './ChatBox'
-import SideBar from './SideBar'
-import SignIn from './SignIn'
-export { Header, Footer, ChatBox, SideBar, SignIn }
